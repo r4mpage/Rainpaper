a6b59d819fae34d5f049c718f2d14ae778cb8ca9
coder <rockst4r98@hotmail.com>
2019-05-03 18:11:46 +1000

    initial commit

---
+++ Rainpaper/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jono
+ * Date: 3/05/2019
+ * Time: 1:49 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Rainpaper
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.ContextMenuStrip menu;
+		private System.Windows.Forms.ToolStripMenuItem chooseNew;
+		private System.Windows.Forms.ToolStripMenuItem exitApplication;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.chooseNew = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitApplication = new System.Windows.Forms.ToolStripMenuItem();
+			this.menu.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.ContextMenuStrip = this.menu;
+			this.notifyIcon.Icon = global::Rainpaper.Resource1.ico;
+			this.notifyIcon.Text = "Paper";
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIconMouseClick);
+			// 
+			// menu
+			// 
+			this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.chooseNew,
+			this.exitApplication});
+			this.menu.Name = "menu";
+			this.menu.Size = new System.Drawing.Size(175, 48);
+			// 
+			// chooseNew
+			// 
+			this.chooseNew.Name = "chooseNew";
+			this.chooseNew.Size = new System.Drawing.Size(174, 22);
+			this.chooseNew.Text = "Choose New Paper";
+			this.chooseNew.Click += new System.EventHandler(this.ChooseNewClick);
+			// 
+			// exitApplication
+			// 
+			this.exitApplication.Name = "exitApplication";
+			this.exitApplication.Size = new System.Drawing.Size(174, 22);
+			this.exitApplication.Text = "Exit";
+			this.exitApplication.Click += new System.EventHandler(this.ExitApplicationClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(200, 200);
+			this.ControlBox = false;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Rainpaper";
+			this.menu.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Rainpaper/MainForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using Gecko;
+
+namespace Rainpaper
+{
+	public partial class MainForm : Form
+	{
+		[DllImport("user32.dll",EntryPoint="FindWindow")]
+		private static extern IntPtr FindWindow(string sClass, string sWindow);
+		[DllImport("user32.dll",EntryPoint="SendMessageTimeout")]
+		private static extern int SendMessageTimeout(IntPtr HWND, uint MSG, IntPtr wParam, IntPtr lParam, uint flags, uint timeout, out IntPtr result);
+		[DllImport("user32")] public static extern int EnumWindows(IntPtr lpEnumFunc, IntPtr lParam);
+		[DllImport("user32", EntryPoint="FindWindowEx")]
+		public static extern IntPtr FindWindowEx(IntPtr hWnd1, IntPtr hWnd2, string lpsz1, string lpsz2);
+		
+		[DllImport("user32.dll")]
+		private static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+		
+		[DllImport("user32")] public static extern IntPtr GetDCEx(IntPtr hwnd, IntPtr hrgnclip, IntPtr fdwOptions);
+		
+		[DllImport("user32")] public static extern int ReleaseDC(IntPtr hwnd, IntPtr hdc);
+		
+		[DllImport("user32")] public static extern int SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+		 
+		public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+		
+		IntPtr workerw = IntPtr.Zero;
+		GeckoWebBrowser geckoWebBrowser;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+			
+			string url = Prompt.ShowDialog("https://filebin.net/6iksluahwplvxto3/dark.htm?t=9viumexr", "Please enter the URL");
+			if(url=="-1") {
+				Environment.Exit(0);
+			}
+			
+			
+			IntPtr progman = FindWindow("Progman", null);
+			IntPtr result = IntPtr.Zero;
+			
+			SendMessageTimeout(progman, 0x052C, new IntPtr(0), IntPtr.Zero, 0, 1000, out result);
+			
+			
+			IntPtr p = IntPtr.Zero;
+			EnumWindows(new EnumWindowsProc((tophandle, topparamhandle) =>
+			{
+			    p = FindWindowEx(tophandle, IntPtr.Zero, "SHELLDLL_DefView", "");
+			
+			    if (p != IntPtr.Zero)
+			    {
+			        workerw = FindWindowEx(IntPtr.Zero, tophandle, "WorkerW", "");
+			    }
+			
+			    return true;
+			}), IntPtr.Zero);
+			
+			this.Width = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+			this.Height = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+			
+			//*
+			Xpcom.Initialize();
+			geckoWebBrowser = new GeckoWebBrowser {Dock = DockStyle.Fill};
+			
+			this.Controls.Add(geckoWebBrowser);
+			
+			if(url != "-1") {
+				geckoWebBrowser.Navigate(url);
+				geckoWebBrowser.Load += (s, e) => {SetParent(this.Handle, workerw);};
+			}else {
+				geckoWebBrowser.Dispose();
+				this.Close();
+				Application.Exit();
+			}
+		}
+
+		void t_Tick(object sender, EventArgs e)
+		{
+			
+			MessageBox.Show("Finished Loading");
+		}
+		
+		void ChooseNewClick(object sender, EventArgs e)
+		{
+			string url = Prompt.ShowDialog("https://filebin.net/6iksluahwplvxto3/dark.htm?t=9viumexr", "Please enter the URL");
+			if(url=="-1") {
+				Environment.Exit(0);
+			}
+			
+			geckoWebBrowser.Navigate(url);
+		}
+		
+		void ExitApplicationClick(object sender, EventArgs e)
+		{
+			Environment.Exit(0);
+		}
+		void NotifyIconMouseClick(object sender, MouseEventArgs e)
+		{
+			MethodInfo mi = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);
+        	mi.Invoke(notifyIcon, null);
+		}
+	}
+	
+	public static class Prompt
+	{
+	    public static string ShowDialog(string text, string caption)
+	    {
+	        Form prompt = new Form()
+	        {
+	            Width = 500,
+	            Height = 150,
+	            FormBorderStyle = FormBorderStyle.FixedDialog,
+	            Text = caption,
+	            StartPosition = FormStartPosition.CenterScreen
+	        };
+	        
+	        TextBox textBox = new TextBox() { Left = 50, Top=10, Width=400 };
+	        textBox.Text = text;
+	        Button confirmation = new Button() { Text = "Ok", Left=50, Width=70, Top=30, DialogResult = DialogResult.Cancel};
+	        Button cancel = new Button() { Text = "Exit", Left=120, Width=70, Top=30, DialogResult = DialogResult.OK };
+	        Button find = new Button() { Text = "Browse", Left=190, Width=70, Top=30, DialogResult = DialogResult.None };
+	        
+	        OpenFileDialog ofd = new OpenFileDialog();
+	        
+	        confirmation.Click += (sender, e) => { prompt.Close(); };
+	        prompt.Controls.Add(textBox);
+	        prompt.Controls.Add(confirmation);
+	        prompt.Controls.Add(cancel);
+	        prompt.Controls.Add(find);
+	        
+	        prompt.AcceptButton = confirmation;
+	        prompt.CancelButton = cancel;
+			
+	        find.Click += (s, e) => {ofd.ShowDialog(); textBox.Text = ofd.FileName;};
+	        
+	        string url = "-1";
+	        
+	        confirmation.Click += (s, e) => {url = textBox.Text;};
+	        
+	        prompt.ShowDialog();
+	        
+	        return url;
+	    }
+	}
+}
